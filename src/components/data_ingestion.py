30b8e39d37318429923433b46614d5098583e531
rahulpalanivel <palanivelrahul45@gmail.com>
2024-04-16 21:32:14 +0530

    mode_trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,6 +3,12 @@ import os
 import pandas as pd
 
 from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
+
+
 from src.exception import CustomException
 from src.logger import logging
 
@@ -47,4 +53,7 @@ if __name__ == "__main__":
     obj = DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion()
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr, _  = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
